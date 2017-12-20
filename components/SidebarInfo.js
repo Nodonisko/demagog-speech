178f6b70594764292c5260869daaa247804bdbb3
Daniel Suchy <suchydan@gmail.com>
2017-12-20 13:30:17 +0100

    Fix date

--- components/SidebarInfo.js
+++ components/SidebarInfo.js
@@ -1,5 +1,8 @@
 // @flow
 import * as React from 'react'
+import moment from 'moment'
+
+moment.locale('cs')
 
 type SidebarInfoProps = {
   publishedAt: string,
@@ -18,7 +21,7 @@ const SidebarInfo = ({
     <ul className="list-group">
       <li className="list-group-item list-group-item-light d-flex justify-content-between align-items-center">
         Publikováno
-        <strong>{new Date(publishedAt).toLocaleDateString('cs-CS')}</strong>
+        <strong>{moment(publishedAt).format('D. MMMM YYYY')}</strong>
       </li>
       <li className="list-group-item list-group-item-light d-flex justify-content-between align-items-center">
         Moderátor
